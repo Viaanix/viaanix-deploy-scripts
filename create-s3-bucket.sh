97a0c1284e7c04ef79a135c85cc95f3d47504a61
KelsiAndrews <kelsi.andrews@viaanix.com>
2024-01-24 10:21:10 -0600

    First Commit!

---
+++ create-s3-bucket.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+
+# Bash Styling
+RESET="\e[0m"
+RESET_COLOR="\e[39m"
+BOLD="\e[1m"
+BLUE="\e[34m${BOLD}"
+RED="${RESET}\e[38;5;196m"
+YELLOW="\e[33m${BOLD}"
+GREEN="\e[32m${BOLD}"
+X="${RESET}${BOLD}[${RED}x${RESET_COLOR}${BOLD}]${RESET}"
+CHECKMARK="${RESET}${BOLD}[${GREEN}✓${RESET_COLOR}]${RESET}"
+
+# Transform Long Arguments to Short Arguments
+for ARG in "$@"; do
+  shift
+  case "$ARG" in
+    '--application-bucket') set -- "$@" '-b' ;;
+    '--application-bucket-name') set -- "$@" '-b' ;;
+    '--bucket') set -- "$@" '-b' ;;
+    '--bucket-name') set -- "$@" '-b' ;;
+    '--tags') set -- "$@" '-t' ;;
+    '--profile') set -- "$@" '-p' ;;
+    *) set -- "$@" "$ARG" ;;
+  esac
+done
+
+# Short Arguments
+while getopts b:t:p: ARG; do
+  case $ARG in
+    b) BUCKET_NAME=$OPTARG ;;
+    t) read -r -a TAGS <<< "$OPTARG" ;;
+    p) PROFILE_ARG=(--profile "$OPTARG") ;;
+#    p) PROFILE=$OPTARG ;;
+    *) usage ;;
+  esac
+done
+
+# Creating S3 Bucket if it Doesn't Exist
+echo -e "${BLUE}Looking for existing S3 Bucket $BUCKET_NAME...${RED}"
+(
+  aws s3api head-bucket --bucket "$BUCKET_NAME" "${PROFILE_ARG[@]}" > /dev/null &&
+  echo -e "${CHECKMARK} S3 Bucket $BUCKET_NAME ${GREEN}found${RESET}"
+) || # Bucket Does Not Exist -> Create Bucket
+  (
+    echo -e "${X} S3 Bucket $BUCKET_NAME not found. Creating an S3 Bucket $BUCKET_NAME...${RED}" &&
+      (
+        aws s3api create-bucket --bucket "$BUCKET_NAME" --region us-east-1 --acl private --profile "${PROFILE_ARG[@]}" > /dev/null &&
+        echo -e "${CHECKMARK} S3 Bucket $BUCKET_NAME ${GREEN}created${RESET}"
+      ) || # Error Creating Bucket
+      echo -e "${X} Error creating the S3 Bucket $BUCKET_NAME"
+  )
+
+# Turning Off All Public Access Settings to the S3 Bucket
+echo -e "${BLUE}Checking public access settings of the S3 Bucket $BUCKET_NAME...${RED}"
+PUBLIC_ACCESS_SETTINGS=$( aws s3api get-public-access-block --bucket "$BUCKET_NAME" "${PROFILE_ARG[@]}" | jq ".[].BlockPublicAcls,.[].IgnorePublicAcls,.[].BlockPublicPolicy,.[].RestrictPublicBuckets")
+
+if echo "$PUBLIC_ACCESS_SETTINGS" | grep -q "false"; then # Some Settings are Not Blocking Public Access
+  echo -e "${X} Some settings are not ${YELLOW}Blocking Public Access${RESET} to the S3 Bucket $BUCKET_NAME. This is not best practice - Blocking public access to the S3 Bucket $BUCKET_NAME..."
+  aws s3api put-public-access-block --bucket "$BUCKET_NAME" --public-access-block-configuration "BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true" "${PROFILE_ARG[@]}"
+elif echo "$PUBLIC_ACCESS_SETTINGS" | grep -q "true"; then # Block Public Access Settings are On
+  echo -e "${CHECKMARK} The S3 Bucket $BUCKET_NAME is ${GREEN}Blocking Public Access${RESET}"
+else # Error Accessing the Public Access Settings of the S3 Bucket
+  echo -e "${X} Error accessing the Blocking Public Access Settings to the S3 Bucket $BUCKET_NAME"
+  echo -e "${BLUE}Creating Block Public Access Settings for the S3 Bucket $BUCKET_NAME...${RED}"
+  (
+    aws s3api put-public-access-block --bucket "$BUCKET_NAME" --public-access-block-configuration "BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true" "${PROFILE_ARG[@]}" &&
+    echo -e "${CHECKMARK} Created Block Public Access Settings for the S3 Bucket $BUCKET_NAME"
+  ) || echo -e "${X} Error creating Block Public Access Settings for the S3 Bucket $BUCKET_NAME"
+fi
+
+# Changing Object Ownership Settings of the S3 Bucket
+echo -e "${BLUE}Checking object ownership settings of the S3 Bucket $BUCKET_NAME...${RED}"
+(
+  aws s3api get-bucket-ownership-controls --bucket "$BUCKET_NAME" "${PROFILE_ARG[@]}" | jq ".[].Rules[].ObjectOwnership" |
+    if grep -q "BucketOwnerPreferred"; then # Bucket Owner Preferred is On
+      echo -e "${CHECKMARK} The object ownership of the S3 Bucket $BUCKET_NAME is ${GREEN}\e[32mBucketOwnerPreferred${RESET}"
+    else
+      (
+        grep -q "BucketOwnerPreferred" && # Checking For Error
+          # Changing the Object Ownership Settings of the S3 Bucket to Bucket Owner Preferred
+          echo -e "${X} The object ownership of the S3 Bucket $BUCKET_NAME is not ${YELLOW}BucketOwnerPreferred${RESET}. This is not best practice - Setting the object ownership of the S3 Bucket $BUCKET_NAME to ${YELLOW}\e[33mBucketOwnerPreferred${RESET}...${RED}" &&
+          aws s3api put-bucket-ownership-controls --bucket "$BUCKET_NAME" --ownership-controls "{\"Rules\":[{\"ObjectOwnership\":\"BucketOwnerPreferred\"}]}" "${PROFILE_ARG[@]}"
+      ) || # Error Accessing the Object Ownership Settings of the S3 Bucket
+        echo -e "${X} Error accessing the object ownership settings of the S3 Bucket $BUCKET_NAME"
+      echo -e "${BLUE}Creating Bucket Ownership Settings for the S3 Bucket $BUCKET_NAME...${RED}"
+      (
+        aws s3api put-bucket-ownership-controls --bucket "$BUCKET_NAME" --ownership-controls "{\"Rules\":[{\"ObjectOwnership\":\"BucketOwnerPreferred\"}]}" "${PROFILE_ARG[@]}" &&
+        echo -e "${CHECKMARK} Created Bucket Ownership Settings for the S3 Bucket $BUCKET_NAME"
+      ) || echo -e "${X} Error creating Bucket Ownership Settings for the S3 Bucket $BUCKET_NAME"
+    fi
+) ||
+  (
+    echo -e "${X} Error accessing the object ownership settings of the S3 Bucket $BUCKET_NAME"
+    echo -e "${BLUE}Creating Bucket Ownership Settings for the S3 Bucket $BUCKET_NAME...${RED}"
+    (
+      aws s3api put-bucket-ownership-controls --bucket "$BUCKET_NAME" --ownership-controls "{\"Rules\":[{\"ObjectOwnership\":\"BucketOwnerPreferred\"}]}" "${PROFILE_ARG[@]}" &&
+        echo -e "${CHECKMARK} Created Bucket Ownership Settings for the S3 Bucket $BUCKET_NAME"
+    ) || echo -e "${X} Error creating Bucket Ownership Settings for the S3 Bucket $BUCKET_NAME"
+  )
+
+echo -e "${BLUE}Adding tags the S3 Bucket $BUCKET_NAME...${RED}"
+
+KEYS=()
+declare -A NEW_TAGS
+
+# Getting the Existing Tags of the S3 Bucket
+#   This step is necessary because SAM creates tags that cannot be removed,
+#   and adding tags will remove tags not included in the tagset list
+readarray -t <<< "$( aws s3api get-bucket-tagging --bucket "$BUCKET_NAME" "${PROFILE_ARG[@]}" | tr -s " " | tr -d "{}[],\t\r\"")" &&
+  if [ -z "${MAPFILE[*]}" ]; then # Error Accessing the Existing Tags of the S3 Bucket
+    echo -e "${X} Error accessing the existing tags to the S3 Bucket $BUCKET_NAME"
+    echo -e "${BLUE}Creating new tagset for the S3 Bucket $BUCKET_NAME...${RED}"
+    TAGSET=""
+    # Adding New Tags
+    for TAG in "${TAGS[@]}"; do
+      if [[ $TAGSET == "" ]]; then
+        TAGSET="{Key=$(echo "$TAG" | cut -d "=" -f1),Value=$(echo "$TAG" | cut -d "=" -f2)}"
+      else
+        TAGSET="$TAGSET,{Key=$(echo "$TAG" | cut -d "=" -f1),Value=$(echo "$TAG" | cut -d "=" -f2)}"
+      fi
+      NEW_TAGS["$(echo "$TAG" | cut -d "=" -f1)"]="$(echo "$TAG" | cut -d "=" -f2)"
+    done
+    TAGSET="[$TAGSET]"
+  else
+    # Parsing Tags
+    for TAG in "${TAGS[@]}"; do
+      TAG="${TAG//\"/}"
+      NEW_TAGS["$(echo "$TAG" | cut -d "=" -f1)"]="$(echo "$TAG" | cut -d "=" -f2)"
+    done
+    # Parsing Existing Tags
+    for ENTRY in "${MAPFILE[@]}"; do
+      if [[ -n $ENTRY ]] && [[ $ENTRY != " " ]]; then
+        if [[ $(echo "$ENTRY" | cut -d " " -f2) == "Key:" ]]; then
+          KEYS+=("$(echo "$ENTRY" | cut -d " " -f3)")
+          NEXT_TAG=$(echo "$ENTRY" | cut -d " " -f3)
+        elif [[ $(echo "$ENTRY" | cut -d " " -f2) == "Value:" ]]; then
+          NEW_TAGS[$NEXT_TAG]=$(echo "$ENTRY" | cut -d " " -f3)
+        fi
+      fi
+    done
+
+    # Updating Existing Tags
+    TAGSET=""
+    for KEY in "${KEYS[@]}"; do
+      if [[ $TAGSET == "" ]]; then
+        TAGSET="$TAGSET{Key=$KEY,Value=${NEW_TAGS[$KEY]}}"
+      else
+        TAGSET="$TAGSET,{Key=$KEY,Value=${NEW_TAGS[$KEY]}}"
+      fi
+    done
+
+    TAGSET="[$TAGSET]"
+  fi
+
+# Adding Tags to the S3 Bucket
+(
+  aws s3api put-bucket-tagging --bucket "$BUCKET_NAME" --tagging "TagSet=$TAGSET" "${PROFILE_ARG[@]}" &&
+  echo -e "${CHECKMARK} Successfully ${GREEN}added tags${RESET} to the S3 Bucket $BUCKET_NAME"
+) || echo -e "${X} Error adding tags to the S3 Bucket $BUCKET_NAME. You will need to check the tags manually through the AWS console to ensure they are correctly added"
---
+++ deploy.sh
@@ -0,0 +1,256 @@
+#!/bin/bash
+
+# Bash Styling
+RESET="\e[0m"
+RESET_COLOR="\e[39m"
+BOLD="\e[1m"
+BLUE="\e[34m${BOLD}"
+RED="${RESET}\e[38;5;196m"
+YELLOW="\e[33m${BOLD}"
+GREEN="\e[32m${BOLD}"
+X="${RESET}${BOLD}[${RED}x${RESET_COLOR}${BOLD}]${RESET}"
+CHECKMARK="${RESET}${BOLD}[${GREEN}✓${RESET_COLOR}]${RESET}"
+
+# Transform Long Arguments to Short Arguments
+for ARG in "$@"; do
+  shift
+  case "$ARG" in
+    '--environment') set -- "$@" '-e' ;;
+    '--force-deploy') set -- "$@" '-d' ;;
+    *) set -- "$@" "$ARG" ;;
+  esac
+done
+
+# Short Arguments
+while getopts e:rbdal ARG; do
+  case $ARG in
+    e) ENVIRONMENT=$OPTARG ;;
+    l) LOCAL_DEPLOYMENT=1 ;;
+    r) UPDATE_ROLE=1 ;;
+    b) UPDATE_S3_BUCKET=1 ;;
+    d) FORCE_DEPLOY=1 ;;
+    *) usage ;;
+  esac
+done
+
+if [ -z "$ENVIRONMENT" ] || [ "$ENVIRONMENT" == " " ]; then
+  ENVIRONMENT="DEV"
+fi
+
+if [ "$1" == "all" ]; then
+  UPDATE_ROLE=1
+  UPDATE_S3_BUCKET=1
+  FORCE_DEPLOY=1
+#  DEPLOY=1
+fi
+
+if [ "$LOCAL_DEPLOYMENT" == 1 ]; then
+  TTY="tty"
+else
+  TTY="tty0"
+fi
+
+get_env_var() {
+  (grep "$1=" "$(echo "$ENVIRONMENT" | tr '[:upper:]' '[:lower:]').env") | sed -e 's/\"//' | cut -d '=' -f2-
+}
+
+if [ "$LOCAL_DEPLOYMENT" == 1 ]; then
+  APPLICATION_NAME="$(get_env_var "APPLICATION_NAME")"
+  RUNNER_ACCOUNT_ID="$(get_env_var "RUNNER_ACCOUNT_ID")"
+  ENVIRONMENT="$(echo "$ENVIRONMENT" | tr '[:lower:]' '[:upper:]')"
+  REGION="$(get_env_var "REGION")"
+  PROFILE="$(get_env_var "PROFILE")"
+  RUNNER_ACCESS_TOKEN="$(get_env_var "RUNNER_ACCESS_TOKEN")"
+  ORG_URL="$(get_env_var "ORG_URL")"
+  OIDC_URL="$(get_env_var "OIDC_URL")"
+  OIDC_THUMBPRINT="$(get_env_var "OIDC_THUMBPRINT")"
+  UNPARSED_TAGS="$(get_env_var "TAGS")"
+fi
+
+if [ -n "$PROFILE" ]; then
+  PROFILE_ARG=("--profile" "$PROFILE")
+fi
+
+check_aws_creds() {
+  echo -e "\n\e[1m\e[38;5;39m* Checking status of AWS Credentials..." > /dev/"$TTY"
+  (
+    aws sts get-caller-identity "${PROFILE_ARG[@]}" > /dev/null &&
+    echo -e "\e[1;32m  Valid Security Token" > /dev/"$TTY"
+  ) || (
+    (
+      # TODO: Add windows vs mac check
+      echo -e "${BOLD}${RED} Expired Security Token\n\e[1m\e[33m  > aws configure sso${YELLOW}" > /dev/"$TTY"
+      winpty aws configure sso "${PROFILE_ARG[@]}" > /dev/"$TTY" &&
+      aws sts get-caller-identity "${PROFILE_ARG[@]}" | jq ".Account" | tr -d "\""
+    ) ||
+    echo -e "${BOLD}${RED}  Error running 'aws configure sso'\n\n\e[1;38;5;177mAttempting to run the tests without AWS Access...\e[1;38;5;39m" > /dev/stderr
+  )
+}
+
+parse_tags() {
+  # Parsing Tags to a Usable Form for the AWS CLI
+
+  if [ "$LOCAL_DEPLOYMENT" == 1 ]; then
+    read -r -a TAGSET <<< "$(get_env_var "TAGS")"
+  else
+    read -r -a TAGSET <<< "$TAGS"
+  fi
+
+  TAGS=""
+  KEYS=()
+
+  declare -A TAGS_FOR_DEPLOY
+
+  for TAG in "${TAGSET[@]}"; do
+    TAG="${TAG//\"/}"
+    if [[ -n $TAG ]] && [[ $TAG != " " ]]; then
+      KEY="$( (echo "$TAG" | cut -d "=" -f1))"
+      VALUE="$( (echo "$TAG" | cut -d "=" -f2) | sed -e 's/'\''//')"
+      if [[ $TAGS == "" ]]; then
+        TAGS="{\"Key\":\"${KEY}\",\"Value\":\"${VALUE}\"}"
+      else
+        TAGS="$TAGS,{\"Key\":\"${KEY}\",\"Value\":\"${VALUE}\"}"
+      fi
+      KEYS+=("$KEY")
+      TAGS_FOR_DEPLOY[$KEY]=$VALUE
+    fi
+  done
+
+  # Updating Existing Tags
+  NEW_TAGSET=""
+  for KEY in "${KEYS[@]}"; do
+    if [[ $NEW_TAGSET == "" ]]; then
+      NEW_TAGSET="$NEW_TAGSET{Key=$KEY,Value=${TAGS_FOR_DEPLOY[$KEY]}}"
+    else
+      NEW_TAGSET="$NEW_TAGSET,{Key=$KEY,Value=${TAGS_FOR_DEPLOY[$KEY]}}"
+    fi
+  done
+
+  NEW_TAGSET="[$NEW_TAGSET]"
+
+  echo "[$TAGS]" "[$NEW_TAGSET]"
+}
+
+AWS_ACCOUNT_ID=$(check_aws_creds)
+LOWERCASE_APPLICATION_NAME="$(echo "$APPLICATION_NAME" | sed -e 's|\([A-Z][^A-Z]\)| \1|g' -e 's|\([a-z]\)\([A-Z]\)|\1 \2|g' | sed 's/^ *//g' | tr '[:upper:]' '[:lower:]' | tr " " "-")-$(echo "$ENVIRONMENT" | tr '[:upper:]' '[:lower:]')"
+SAM_MANAGED_BUCKET="$(echo "$APPLICATION_NAME" | sed -e 's|\([A-Z][^A-Z]\)| \1|g' -e 's|\([a-z]\)\([A-Z]\)|\1 \2|g' | sed 's/^ *//g' | tr '[:upper:]' '[:lower:]' | tr " " "-")-sam-managed-$(echo "$ENVIRONMENT" | tr '[:upper:]' '[:lower:]')"
+
+if [ "$LOCAL_DEPLOYMENT" != 1 ]; then
+  UNPARSED_TAGS=$TAGS
+fi
+
+read -r -a TAGS <<< "$(parse_tags)"
+
+update_role() {
+  ./deploy-scripts/create-deploy-role.sh --aws-account-id "$AWS_ACCOUNT_ID" \
+          --github-runner-account-id "$RUNNER_ACCOUNT_ID" \
+          --application-name "$APPLICATION_NAME" \
+          --lowercase-application-name "$LOWERCASE_APPLICATION_NAME" \
+          --environment "$ENVIRONMENT" \
+          --region "$REGION" \
+          "${PROFILE_ARG[@]}" \
+          --sam-managed-bucket "$SAM_MANAGED_BUCKET" \
+          --oidc-url "$OIDC_URL" \
+          --tags "${TAGS[*]}"
+}
+
+update_s3_bucket() {
+  ./deploy-scripts/create-s3-bucket.sh --bucket-name "$SAM_MANAGED_BUCKET" --tags "$UNPARSED_TAGS" "${PROFILE_ARG[@]}"
+}
+
+deploy_sam() {
+  if where sam 2> /dev/null | grep -qi '.cmd'; then
+    echo -e "\n\e[1m\e[38;5;39m* Deploying to AWS through SAM..."
+    C:/PROGRA~1/Amazon/AWSSAMCLI/bin/sam.cmd deploy \
+    "${PROFILE_ARG[@]}" \
+    --parameter-overrides "ApplicationName=$APPLICATION_NAME Environment=$ENVIRONMENT Region=$REGION LowerCaseApplicationName=$LOWERCASE_APPLICATION_NAME OrgURL=$ORG_URL GitHubRunnerAccessToken=$RUNNER_ACCESS_TOKEN GitHubOIDCURL=$OIDC_URL GitHubOIDCThumbprint=$OIDC_THUMBPRINT $UNPARSED_TAGS" \
+    --stack-name "${APPLICATION_NAME}${ENVIRONMENT}" \
+    --s3-bucket "${SAM_MANAGED_BUCKET}" \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --region "$REGION" \
+    --tags "$UNPARSED_TAGS" \
+    --no-fail-on-empty-changeset
+  else
+    sam deploy \
+    "${PROFILE_ARG[@]}" \
+    --parameter-overrides "ApplicationName=$APPLICATION_NAME Environment=$ENVIRONMENT Region=$REGION LowerCaseApplicationName=$LOWERCASE_APPLICATION_NAME OrgURL=$ORG_URL GitHubRunnerAccessToken=$RUNNER_ACCESS_TOKEN GitHubOIDCURL=$OIDC_URL GitHubOIDCThumbprint=$OIDC_THUMBPRINT $UNPARSED_TAGS" \
+    --stack-name "${APPLICATION_NAME}${ENVIRONMENT}" \
+    --s3-bucket "${SAM_MANAGED_BUCKET}" \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --region "$REGION" \
+    --tags "$UNPARSED_TAGS" \
+    --no-fail-on-empty-changeset
+  fi
+}
+
+if [ "$UPDATE_ROLE" == 1 ]; then
+  update_role
+fi
+
+if [ "$UPDATE_S3_BUCKET" == 1 ]; then
+  update_s3_bucket
+fi
+
+if [ "$FORCE_DEPLOY" == 1 ] && [ "$LOCAL_DEPLOYMENT" != 1 ]; then
+    deploy_sam
+fi
+
+if [ "$LOCAL_DEPLOYMENT" == 1 ]; then
+  readarray -t DEPLOY_FILES <<< "$( (stat -c %n:%Y ./deploy-scripts/deploy.sh "$(echo "$ENVIRONMENT" | tr '[:upper:]' '[:lower:]').env" ./deploy-scripts/create-* template.yaml) | sed -e 's/.\/deploy-scripts\///' | sed -e 's/.sh//')"
+
+  LAST_DEPLOY=0
+  MODIFIED=0
+
+  for FILE in "${DEPLOY_FILES[@]}"; do
+    FILE_NAME="$(echo "$FILE" | cut -d ':' -f1)"
+    MODIFIED_TIME="$(echo "$FILE" | cut -d ':' -f2)"
+
+    case "$FILE_NAME" in
+    "deploy")
+      LAST_DEPLOY="$MODIFIED_TIME"
+      ;;
+  #    export LAST_DEPLOY ;;
+    "$(echo "$ENVIRONMENT" | tr '[:upper:]' '[:lower:]').env")
+      if [[ "$MODIFIED_TIME" -gt "$LAST_DEPLOY" ]]; then
+        update_role
+        update_s3_bucket
+        MODIFIED=1
+      fi
+      ;;
+    "create-deploy-role")
+      if [[ "$MODIFIED_TIME" -gt "$LAST_DEPLOY" ]]; then
+        update_role
+        MODIFIED=1
+      fi
+      ;;
+    "create-s3-bucket")
+      if [[ "$MODIFIED_TIME" -gt "$LAST_DEPLOY" ]]; then
+        update_s3_bucket
+        MODIFIED=1
+      fi
+      ;;
+    "template.yaml")
+      if [[ "$MODIFIED_TIME" -gt "$LAST_DEPLOY" ]]; then
+        MODIFIED=1
+      fi
+      ;;
+    *) ;;
+  #    echo "Unexpected File - skipping" ;;
+    esac
+
+  done
+
+  if [ "$MODIFIED" == 1 ]; then
+    deploy_sam
+  else
+    if [ "$FORCE_DEPLOY" == 1 ]; then
+      deploy_sam
+    else
+      echo -e "\n\e[1m\e[38;5;39mNo Modified Files\nUse --force-deploy or -d to Force a Deployment\n\nExiting..."
+    fi
+  fi
+
+  touch ./deploy-scripts/create-*.sh
+  touch "$(echo "$ENVIRONMENT" | tr '[:upper:]' '[:lower:]').env"
+  touch ./deploy-scripts/deploy.sh
+fi
