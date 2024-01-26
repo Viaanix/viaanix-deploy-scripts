c96acdb73e4b076f69b6fbe9a04e240835e44e3c
KelsiAndrews <kelsi.andrews@viaanix.com>
2024-01-26 11:46:31 -0600

    Making Deploy Scripts Executable

--- deploy-scripts/deploy.sh
+++ deploy-scripts/deploy.sh
@@ -52,6 +52,26 @@ fi
 
 echo "Echoing Arguments and Exiting for Testing..."
 echo "$@"
+# APPLICATION_NAME
+echo "$APPLICATION_NAME"
+#       RUNNER_ACCOUNT_ID
+echo "$RUNNER_ACCOUNT_ID"
+#       ENVIRONMENT
+echo "$ENVIRONMENT"
+#       REGION
+echo "$REGION"
+#       PROFILE
+echo "$PROFILE"
+#       RUNNER_ACCESS_TOKEN
+echo "$RUNNER_ACCESS_TOKEN"
+#       ORG_URL
+echo "$ORG_URL"
+#       OIDC_URL
+echo "$OIDC_URL"
+#       OIDC_THUMBPRINT
+echo "$OIDC_THUMBPRINT"
+#       UNPARSED_TAGS
+echo "$UNPARSED_TAGS"
 exit
 
 get_env_var() {
